7d88ffcafb200e6533ee3bee01a2f3be8f74f76a
george <georgedimac@gmail.com>
2022-09-20 10:42:44 +0300

    Update api.service.ts
    new api scheme

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -28,25 +28,30 @@ export class ContactsApiService extends ApiService {
             .set('page', '1')
             .set('pageSize', '10');
         //return this.http.get<Contact[]>(`${this.baseUrl}/getallcontact`, {params});
-        return this.http.get<Contact[]>(`${this.baseUrl}getallcontact`);
+        //return this.http.get<Contact[]>(`${this.baseUrl}getallcontact`);
+        return this.http.get<Contact[]>(this.baseUrl);
     }
 
     get(id: number) {
-        return this.http.get<Contact>(`${this.baseUrl}getcontact?id=${id}`);
+        //return this.http.get<Contact>(`${this.baseUrl}getcontact?id=${id}`);
+        return this.http.get<Contact>(`${this.baseUrl}${id}`);
     }
 
     create(contact: Contact) {
         const headers = this.getHeaders();
-        return this.http.post<Contact>(`${this.baseUrl}addcontact`, contact, {headers});
+        //return this.http.post<Contact>(`${this.baseUrl}addcontact`, contact, {headers});
+        return this.http.post<Contact>(this.baseUrl, contact, {headers});
     }
 
     update(contact: Contact) {
         const headers = this.getHeaders();
-        return this.http.put<Contact>(`${this.baseUrl}updatecontact?id=${contact.id}`, contact, {headers});
+        //return this.http.put<Contact>(`${this.baseUrl}updatecontact?id=${contact.id}`, contact, {headers});
+        return this.http.put<Contact>(`${this.baseUrl}${contact.id}`, contact, {headers});
     }
 
     delete(id: number) {
         const headers = this.getHeaders();
-        return this.http.delete(`${this.baseUrl}deletecontact?id=${id}`, {headers});
+        //return this.http.delete(`${this.baseUrl}deletecontact?id=${id}`, {headers});
+        return this.http.delete(`${this.baseUrl}${id}`, {headers});
     }
 }
